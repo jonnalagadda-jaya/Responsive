f6852c5ea0b3af87faf3c6fe7fc8d0f48e9f05ca
Jayasri <jayasri.jonnalagadda@srinine.com>
2024-08-12 13:42:58 +0530

    firstcommit

--- src/component/Contact.tsx
+++ src/component/Contact.tsx
@@ -7,11 +7,17 @@ const Contact = () => {
         <div className="text-2xl m-20">
         <p><strong>E-Mail: </strong>jaya.jonnalagadda0089@gmail.com</p>
         <p><strong>Mobile Number:</strong> 7729849818</p>
-        <p> <strong>LnkedIn:</strong><a href="https://www.linkedin.com/in/jayasri-jonnalagadda-241b2b198/">Click Here</a> </p>
+          <p> <strong>LnkedIn:</strong><a href="https://www.linkedin.com/in/jayasri-jonnalagadda-241b2b198/">Click Here</a> </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ullam maxime nesciunt repudiandae consequuntur excepturi
+            quae perferendis tenetur ipsa voluptas nemo quaerat,
+            modi officia voluptatibus ipsam et adipisci? Cupiditate, perferendis.</p>
+          <p className="text-red-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ex similique, minima repellat exercitationem incidunt nulla
+            velit accusamus minus perferendis corporis
+            eligendi consequuntur delectus quasi obcaecati modi! Inventore, a delectus?</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default Contact
+export default Contact;
